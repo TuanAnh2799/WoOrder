f5c65cf10db7553dd1e2c00c99264f268e32fbb9
“Tuan <Tuananh@gmail.com>
2022-02-08 12:55:57 +0700

    commit chart

--- components/Screens/Profile/chart/statistical.js
+++ components/Screens/Profile/chart/statistical.js
@@ -4,7 +4,10 @@ import firestore from '@react-native-firebase/firestore';
 import LinearGradient from 'react-native-linear-gradient';
 import {Picker} from '@react-native-picker/picker';
 import formatCash from '../../API/ConvertPrice';
-
+import {PieChart} from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+  
+const screenWidth = Dimensions.get("window").width;
 
 const statisticalScreen = () => {
 
@@ -170,39 +173,110 @@ useEffect(async () => {
         for (let i = d.getFullYear(); i >= 2021; i--) {
             listYear.push(<Picker.Item key={i} value={i.toString()} label={i.toString()} />);}
 
+    const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+    };
+
+    const data = [
+        {
+          name: "Công nghệ",
+          population: type1,
+          color: "red",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Thời trang",
+          population: type2,
+          color: "green",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Đồ chơi",
+          population: type3,
+          color: "orange",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        
+      ];
+
+      const data2 = [
+        {
+          name: "Công nghệ",
+          population: typeY1,
+          color: "red",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Thời trang",
+          population: typeY2,
+          color: "green",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Đồ chơi",
+          population: typeY3,
+          color: "orange",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        
+      ];
+
+
     return (
         <View style={{width: '100%', height: '100%'}}>
             <ScrollView style={{flex: 1}}>
-                <View style={{flex:1.2}}>
+                <View>
                     <View style={{width: '80%', justifyContent:'center', alignItems:'center', marginLeft:'10%',height: 40,marginTop:10, borderColor:'#fff', backgroundColor:'#fff',borderWidth: 0.5, borderRadius: 10, shadowColor:'black', elevation: 5 }}>
                         <Text style={{fontSize: 18, marginBottom: 10, marginTop: 5}}>Trong ngày</Text>
                     </View>
 
-                    <View style={{width: '100%', flexDirection:'row', justifyContent:'space-around', marginTop: 10}}>
-                        <LinearGradient colors={['#ffff', 'yellow', '#fff']} style={{width: '40%',height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 30}}>15</Text>
-                            <Text style={{fontSize: 15}}>Đơn hàng</Text>
-                        </LinearGradient>
-
-                        <LinearGradient colors={['red', 'white', 'green']} style={{width: '40%', height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 18, marginTop: 15}}>14.000.000 VNĐ</Text>
-                            <Text style={{fontSize: 15, marginTop: 5}}>Tổng tiền</Text>
-                        </LinearGradient>
-
+                    <View style={{width: '96%', height: 200, backgroundColor:'#fff',shadowColor:'black', elevation:5 , marginLeft:'2%', marginTop: 15, borderTopLeftRadius: 100, borderBottomRightRadius: 100}}>
+                        {
+                            day.length > 0 ? 
+                            (<View style={{width: '100%', height: '100%', justifyContent:'center', alignItems:'center'}}>
+                                <Text style={{fontSize: 18, color:'green', fontWeight: '700'}}>Hôm nay không có đơn hàng nào :(((</Text>
+                            </View>)
+                            :
+                            (<View style={{width: '100%', height: '100%'}}>
+                                <View style={{width: '100%', height: '100%', flexDirection:'row', justifyContent:'space-around', alignItems:'center'}}>
+                                    <View style={{width:'38%', height: 100, backgroundColor:'rgb(135,206,235)', shadowColor:'black', elevation:5 , borderTopLeftRadius: 50, borderBottomRightRadius: 50, justifyContent:'center', alignItems:'center'}}>
+                                        <Text style={{fontSize: 30}}>15</Text>
+                                        <Text style={{fontSize: 15}}>Đơn hàng</Text>
+                                    </View>
+                                    <View style={{width:'50%', height: 100, backgroundColor:'orange',shadowColor:'black', elevation:5 , borderTopLeftRadius: 50, borderBottomRightRadius: 50, justifyContent:'center', alignItems:'center' }}>
+                                        <Text style={{fontSize: 18, marginTop: 15, color:'black'}}>14.000.000 VNĐ</Text>
+                                        <Text style={{fontSize: 15, marginTop: 5}}>Tổng tiền</Text>
+                                    </View>
+                                </View>
+                            </View>)
+                        }
                     </View>
 
                 </View>
 
-                <View style={{flex:2,}}>
-                    <View style={{width: '80%', justifyContent:'center', alignItems:'center', backgroundColor:'#fff',marginLeft:'10%', borderWidth: 0.5, borderColor:'#fff',borderRadius: 10, shadowColor:'black', elevation: 5}}>
+                <View style={{marginTop: 20}}>
+                    <View style={styles.wrappContentMonth}>
                         <Text style={{fontSize: 18, marginBottom: 10, marginTop: 5}}>Tháng</Text>
                     </View>
 
-                    <View style={{flexDirection:'row', alignItems:'center', marginLeft:'50%', marginTop: 20, marginBottom: 10}}>
+                    <View style={{flexDirection:'row', alignItems:'center', marginLeft:'42%', marginTop: 20, marginBottom: 10}}>
                         <View>
-                            <Text style={{fontSize: 17, }}>Tháng: </Text>
+                            <Text style={{fontSize: 17, }}>Chọn tháng: </Text>
                         </View>
-                        <View style={{width: 100, borderWidth: 0.5, height: 35, justifyContent:'center', borderRadius: 10}}>
+                        <View style={styles.wrapPickerMonth}>
                             <Picker
                                 style={{height: 35, width: 100, padding: 5}}
                                 selectedValue={selectedMonth}
@@ -215,29 +289,57 @@ useEffect(async () => {
                     </View>
 
                     <View style={{width: '100%', flexDirection:'row', justifyContent:'space-around'}}>
-                        <LinearGradient colors={['#ffff', 'yellow', '#fff']} style={{width: '40%',height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 30}}>{countMonth}</Text>
-                            <Text style={{fontSize: 15}}>Đơn hàng</Text>
+                        <LinearGradient colors={['#2974FA', '#38ABFD', '#43D4FF']} style={{width: '40%',height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
+                            <Text style={{fontSize: 30, color:'white'}}>{countMonth}</Text>
+                            <Text style={{fontSize: 15, color:'black'}}>Đơn hàng</Text>
                         </LinearGradient>
 
-                        <LinearGradient colors={['red', 'white', 'green']} style={{width: '40%', height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 18, marginTop: 15}}>{formatCash(type1 + type2 +type3)} VNĐ</Text>
-                            <Text style={{fontSize: 15, marginTop: 5}}>Tổng tiền</Text>
+                        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={{width: '40%', height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
+                            <Text style={{fontSize: 18, marginTop: 15, color:'white'}}>{formatCash(type1 + type2 +type3)} VNĐ</Text>
+                            <Text style={{fontSize: 15, marginTop: 5, color:'white'}}>Tổng tiền</Text>
                         </LinearGradient>
 
                     </View>
+
+                    <PieChart
+                        data={data}
+                        width={screenWidth}
+                        style={{marginTop: 10}}
+                        height={220}
+                        chartConfig={chartConfig}
+                        accessor={"population"}
+                        backgroundColor={"transparent"}
+                        paddingLeft={"10"}
+                        center={[5, -10]}
+                        absolute={false}
+                        />
+
+                    <View style={{width: '100%', alignItems:'center'}}>
+                        <View style={{flexDirection:'row', justifyContent:'space-around'}}>
+                            <View style={styles.wrappMonth1}>
+                                <Text style={styles.textCashMonth}>{formatCash(type1)}<Text style={{color:'white', marginRight:5, fontWeight:'700', marginLeft: 5}}>  VNĐ</Text></Text>
+                            </View>
+                            <View style={styles.wrappMonth2}>
+                                <Text style={styles.textCashMonth}>{formatCash(type2)}<Text style={{color:'white', marginRight:5, fontWeight:'700', marginLeft: 5}}>  VNĐ</Text></Text>
+                            </View>
+                        </View>
+                        <View style={styles.wrappMonth3}>
+                            <Text style={styles.textCashMonth}>{formatCash(type3)}<Text style={{color:'white', marginRight:5, fontWeight:'700', marginLeft: 5}}>  VNĐ</Text></Text>
+                        </View>
+                    </View>
+
                 </View>
 
-                <View style={{flex:2,}}>
-                    <View style={{width: '80%', justifyContent:'center', alignItems:'center', backgroundColor:'#fff',marginLeft:'10%', borderWidth: 0.5, borderColor:'#fff',borderRadius: 10, shadowColor:'black', elevation: 5}}>
+                <View style={{marginTop: 30,}}>
+                    <View style={styles.wrappContentYear}>
                         <Text style={{fontSize: 18, marginBottom: 10, marginTop: 5}}>Năm</Text>
                     </View>
 
-                    <View style={{flexDirection:'row', alignItems:'center', marginLeft:'50%', marginTop: 20, marginBottom: 10}}>
+                    <View style={{flexDirection:'row', alignItems:'center', marginLeft:'40%', marginTop: 20, marginBottom: 10}}>
                         <View>
-                            <Text style={{fontSize: 17, }}>Năm: </Text>
+                            <Text style={{fontSize: 17, }}>Chọn năm: </Text>
                         </View>
-                        <View style={{width: 120, borderWidth: 0.5, height: 35, justifyContent:'center', borderRadius: 10}}>
+                        <View style={styles.wrapPickerYear}>
                             <Picker
                                 style={{height: 35, width: 120, padding: 5}}
                                 selectedValue={selectedYear}
@@ -250,15 +352,45 @@ useEffect(async () => {
                     </View>
 
                     <View style={{width: '100%', flexDirection:'row', justifyContent:'space-around'}}>
-                        <LinearGradient colors={['#ffff', 'yellow', '#fff']} style={{width: '40%',height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 30}}>{countYear}</Text>
-                            <Text style={{fontSize: 15}}>Đơn hàng</Text>
+                        <LinearGradient colors={['#2974FA', '#38ABFD', '#43D4FF']} style={{width: '40%',height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
+                            <Text style={{fontSize: 30, color:'white'}}>{countYear}</Text>
+                            <Text style={{fontSize: 15, color:'black'}}>Đơn hàng</Text>
                         </LinearGradient>
 
-                        <LinearGradient colors={['red', 'white', 'green']} style={{width: '40%', height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
-                            <Text style={{fontSize: 18, marginTop: 15}}>{formatCash(typeY1 + typeY2 + typeY3)} VNĐ</Text>
-                            <Text style={{fontSize: 15, marginTop: 5}}>Tổng tiền</Text>
+                        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={{width: '40%', height: 100,justifyContent:'center', alignItems:'center', borderRadius: 10}}>
+                            <Text style={{fontSize: 18, marginTop: 15, color:'white'}}>{formatCash(typeY1 + typeY2 + typeY3)} VNĐ</Text>
+                            <Text style={{fontSize: 15, marginTop: 5, color: 'white'}}>Tổng tiền</Text>
                         </LinearGradient>
+
+                    </View>
+
+                    <PieChart
+                        data={data2}
+                        width={screenWidth}
+                        style={{marginTop: 10}}
+                        height={220}
+                        chartConfig={chartConfig}
+                        accessor={"population"}
+                        backgroundColor={"transparent"}
+                        paddingLeft={"10"}
+                        center={[5, -10]}
+                        absolute={false}
+                        />
+
+                    <View style={{width: '100%', alignItems:'center'}}>
+                        <View style={{flexDirection:'row', justifyContent:'space-around'}}>
+                            <View style={styles.wrappYear1}>
+                                <Text style={{color: 'white', fontSize: 17}}>{formatCash(typeY1)}<Text style={styles.textYear}>  VNĐ</Text></Text>
+                            </View>
+                            <View style={styles.wrappYear2}>
+                                <Text style={{color: 'white', fontSize: 17}}>{formatCash(typeY2)}<Text style={styles.textYear}>  VNĐ</Text></Text>
+                            </View>
+                        </View>
+                        <View style={styles.wrappYear3}>
+                            <Text style={{color: 'white', fontSize: 17}}>{formatCash(typeY3)}<Text style={styles.textYear}>  VNĐ</Text></Text>
+                        </View>
+                    </View>
+                    <View style={{height: 55}}>
 
                     </View>
                 </View>
@@ -269,4 +401,111 @@ useEffect(async () => {
 
 export default statisticalScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    //css year
+    wrappYear1: {
+        width: '45%',
+        backgroundColor: "red",
+        height: 40, alignItems:'center',
+        justifyContent:'center',
+        borderRadius: 10,
+    },
+    wrappYear2: {
+        width: '45%',
+        backgroundColor: 'green',
+        height: 40,
+        borderRadius: 10,
+        marginLeft: 5,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    wrappYear3: {
+        width: '45%',
+        backgroundColor: 'orange',
+        marginTop: 5,
+        height: 40,
+        borderRadius: 10,
+        marginLeft: 5,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    textYear: {
+        color:'white',
+        marginRight:5,
+        fontWeight:'700',
+        marginLeft: 5
+    },
+    wrappContentYear: {
+        width: '80%',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#fff',
+        marginLeft:'10%',
+        borderWidth: 0.5,
+        borderColor:'#fff',
+        borderRadius: 10,
+        shadowColor:'black',
+        elevation: 5
+    },
+    wrapPickerYear: {
+        width: 120,
+        borderWidth: 0.5,
+        height: 35,
+        justifyContent:'center',
+        borderRadius: 10
+    },
+    //end css year
+
+    // css month
+    wrappMonth1: {
+        width: '45%',
+        backgroundColor: "red",
+        height: 40,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius: 10
+    },
+    wrappMonth2: {
+        width: '45%',
+        backgroundColor: 'green',
+        height: 40,
+        borderRadius: 10,
+        marginLeft: 5,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    wrappMonth3: {
+        width: '45%',
+        backgroundColor: 'orange',
+        marginTop: 5,
+        height: 40,
+        borderRadius: 10,
+        marginLeft: 5,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    textCashMonth: {
+        color: 'white',
+        fontSize: 17
+    },
+    wrapPickerMonth: {
+        width: 100,
+        borderWidth: 0.5,
+        height: 35,
+        justifyContent:'center',
+        borderRadius: 10
+    },
+    wrappContentMonth: {
+        width: '80%',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#fff',
+        marginLeft:'10%',
+        borderWidth: 0.5,
+        borderColor:'#fff',
+        borderRadius: 10,
+        shadowColor:'black',
+        elevation: 5
+    },
+    
+})
